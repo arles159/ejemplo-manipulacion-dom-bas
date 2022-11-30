601eb51c8269d7755533c4baed4dda619e07f025
¨Franco¨ <arles159@hotmail.com>
2022-11-29 23:21:36 -0500

    Funciones de alternancia para los menu

--- proyecto/MarketPlace/script/script.js
+++ proyecto/MarketPlace/script/script.js
@@ -4,118 +4,176 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail2');
-const isAsideClosed = aside.classList.contains('inactive');
-const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+const productDetailContainer = document.querySelector('.product-detail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() { 
+    const isAsideClosed = aside.classList.contains('inactive');
+    
     if(isAsideClosed) {
         aside.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');        
 }
+
 function toggleMobileMenu() {
-    if(!isAsideClosed) {
-        aside.classList.add('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
     }
-    mobileMenu.classList.toggle('inactive');        
+
+    closeProductDetailAside();
+    
+    mobileMenu.classList.toggle('inactive');
 }
+
 function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
     if(!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
+    }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
     }
     aside.classList.toggle('inactive');
 }
 
-const productList = [];
-productList.push(
-    {
-        name: 'Detective Pikachu Doll',
-        price: 120.00,
-        image: 'img\pexels-carolina-castilla-arias-1716861.jpg'
-    });
-productList.push(
-    {
-        name: 'Pokeball life-size',
-        price: 80.00,
-        image: 'img\pexels-vincent-ma-janssen-1310847.jpg'
-    },
-    {
-        name: 'Detective Pikachu Doll',
-        price: 120.00,
-        image: 'img\pexels-carolina-castilla-arias-1716861.jpg'
-    });
-productList.push(
-    {
-        name: 'Pokeball life-size',
-        price: 80.00,
-        image: 'img\pexels-vincent-ma-janssen-1310847.jpg'
-    }
-);
-
-{/* <div class="product-card">
-                <img src="img\pexels-carolina-castilla-arias-1716861.jpg" alt="" class="product-img">
-                <div class="product-info">
-                    <div>
-                        <p>$120,00</p>
-                        <p>Detective Pikachu Doll</p>
-                    </div>
-                    <figure>
-                        <img src="./Icons/bt_add_to_cart.svg" alt="">
-                    </figure>
-                </div>
-            </div>
-            <div class="product-card">
-                <img src="img\pexels-vincent-ma-janssen-1310847.jpg" alt="" class="product-img">
-                <div class="product-info">
-                    <div>
-                        <p>$80,00</p>
-                        <p>Pokeball life-size</p>
-                    </div>
-                    <figure>
-                        <img src="./Icons/bt_add_to_cart.svg" alt="">
-                    </figure>
-                </div>
-            </div>
-        </div> */}
-
-for (product of productList) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-    
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src', 'productList.image');
-
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-    
-    const productInfoDiv = document.createElement('div');
-    
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-    
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
-
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-
-    const productInfoFigure = document.createElement('figure');
-    const productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src', './Icons/bt_add_to_cart.svg');
-
-    productInfoFigure.appendChild(productImgCart);
-
-    productInfo.append(productInfoDiv, productInfoFigure);
-
-    productCard.append(productImg, productInfo);
-
-    cardsContainer.appendChild(productCard);
+function openProductDetailAside() {
+    aside.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
 }
 
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+}
 
-console.log(productList)
+const productList = [];
+productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+    name: 'Detective Pikachu Doll',
+    price: 120.00,
+    image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+});
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+    productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+    name: 'Detective Pikachu Doll',
+    price: 120.00,
+    image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+});
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+productList.push({
+        name: 'Detective Pikachu Doll',
+        price: 120.00,
+        image: './img/pexels-carolina-castilla-arias-1716861.jpg'
+    });
+productList.push({
+        name: 'Pokeball life-size',
+        price: 80.00,
+        image: './img/pexels-vincent-ma-janssen-1310847.jpg'
+    });
+
+function renderProducts(arr) {
+    for(product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;        
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './Icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+renderProducts(productList)
